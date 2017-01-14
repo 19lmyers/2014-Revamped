e1c1e6ea833dcc4142566f69ef848c6e88ffa587
Luke Myers <19lmyers@gmail.com>
2017-01-14 15:52:54 -0800

    Test changes, part 1

--- src/main/java/com/team980/robot2014revamped/Robot.java
+++ src/main/java/com/team980/robot2014revamped/Robot.java
@@ -1,5 +1,6 @@
 package com.team980.robot2014revamped;
 
+import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 
@@ -9,28 +10,38 @@ public class Robot extends IterativeRobot {
 
     private ShiftDrive shiftDrive;
 
+    private Compressor compressor;
+
     @Override
     public void robotInit() {
         joystick = new Joystick(Parameters.DRIVE_JOYSTICK_CHANNEL);
 
         shiftDrive = new ShiftDrive();
+
+        compressor = new Compressor(Parameters.PCM_CAN_ID);
+        compressor.setClosedLoopControl(true);
     }
 
     @Override
     public void teleopInit() {
-        System.out.println("back into the fray");
+        System.out.println("back into the fray v3");
 
     }
 
     @Override
     public void teleopPeriodic() {
-        shiftDrive.drive(joystick); //Drives robot
-
         if (joystick.getRawButton(3)) { //3 manually enables high gear
             shiftDrive.setHighGear(true);
         } else if (joystick.getRawButton(4)) { //4 manually disables high gear
             shiftDrive.setHighGear(false);
         }
+
+        shiftDrive.drive(joystick); //Drives robot
+
+        System.out.println(compressor.enabled() + "");
+        System.out.println(compressor.getPressureSwitchValue() + "");
+        System.out.println(compressor.getCompressorCurrent() + "");
+        System.out.println("");
     }
 
     @Override
